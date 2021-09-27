6266e8d8069189255621362534cff9672482fcac
vaibhavrock <vaibhavupadhyay28@gmail.com>
2021-09-27 10:55:52 +0530

    Update WifiListViewHolder.kt
    fixed on click only wifi name issue

--- app/src/main/java/app/poc/wifi/WifiListViewHolder.kt
+++ app/src/main/java/app/poc/wifi/WifiListViewHolder.kt
@@ -2,19 +2,21 @@ package app.poc.wifi
 
 import android.net.wifi.ScanResult
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class WifiListViewHolder(view: View, val viewHolderCallback: ViewHolderCallback): RecyclerView.ViewHolder(view) {
+class WifiListViewHolder(view: View, private val viewHolderCallback: ViewHolderCallback): RecyclerView.ViewHolder(view) {
 
-    val tvWifiName: TextView = view.findViewById(R.id.tvWifiName)
-    val tvWifiType: TextView = view.findViewById(R.id.tvWifiNameSecondary)
+    private val rootLayout: LinearLayout = view.findViewById(R.id.rootLayout)
+    private val tvWifiName: TextView = view.findViewById(R.id.tvWifiName)
+    private val tvWifiType: TextView = view.findViewById(R.id.tvWifiNameSecondary)
 
     fun setData(data: ScanResult){
         tvWifiName.text = data.SSID
         tvWifiType.text = Common.checkWifiType(data.capabilities)
 
-        tvWifiName.setOnClickListener {
+        rootLayout.setOnClickListener {
             viewHolderCallback.onNetworkClick(data)
         }
     }
